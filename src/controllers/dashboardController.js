bd361c7b26c41b79257a153284253093d777be32
Dalibor Pavlov <pavlov.dalibor@hotmail.com>
2023-02-08 00:13:58 +0100

    Kinda did the topics

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -10,7 +10,8 @@ const showDashboard = async (req, res) => {
     // get username from token
     const { email } = jwt.decode(token);
     const user = await userModel.findOne({ email: email });
-    res.render("dashboard", { user: user, id: "0" });
+    const allTopics = await topicModel.find();
+    res.render("dashboard", { user: user, id: "0", topics: allTopics });
   } else {
     res.redirect("/start");
   }
@@ -22,12 +23,11 @@ const newTopic = async (req, res) => {
   if (!token) return res.redirect("/start");
 
   const { email } = jwt.decode(token);
-  console.log(email);
+
   const teacher = await userModel.findOne({
     email: email,
     isTeacher: true,
   });
-  console.log(teacher);
 
   const topicName = req.body["topic-name"];
 
@@ -41,10 +41,38 @@ const newTopic = async (req, res) => {
     students: students.map((s) => s._id),
   });
 
+  const allTopics = await topicModel.find();
+
   return res.render("dashboard", {
     id: topic._id,
     user: teacher,
+    topics: allTopics,
   });
 };
 
-module.exports = { showDashboard, newTopic };
+const deleteTopic = async (req, res) => {
+  const { id } = req.body;
+
+  const token = req.cookies.token;
+
+  if (!token) return res.redirect("/start");
+
+  const { email } = jwt.decode(token);
+
+  const teacher = await userModel.findOne({
+    email: email,
+    isTeacher: true,
+  });
+
+  await topicModel.deleteMany({ name: id });
+
+  const allTopics = await topicModel.find();
+
+  return res.render("dashboard", {
+    user: teacher,
+    topics: allTopics,
+    id: "0",
+  });
+};
+
+module.exports = { showDashboard, newTopic, deleteTopic };
--- src/routes/dashboardRouter.js
+++ src/routes/dashboardRouter.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { showDashboard, newTopic } = require("../controllers/dashboardController");
+const {
+  showDashboard,
+  newTopic,
+  deleteTopic,
+} = require("../controllers/dashboardController");
 
 const dashboardRouter = express.Router();
 
 dashboardRouter.get("/", showDashboard);
 
-dashboardRouter.post("/new-topic", newTopic)
+dashboardRouter.post("/new-topic", newTopic);
+dashboardRouter.get("/new-topic", (req, res) => res.redirect("/"));
+
+dashboardRouter.post("/delete-topic", deleteTopic);
+dashboardRouter.get("/delete-topic", (req, res) => res.redirect("/"));
 
 module.exports = dashboardRouter;
